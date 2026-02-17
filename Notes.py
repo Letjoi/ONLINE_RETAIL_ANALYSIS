6d8418f5ce2fd12224151f7cae98fb8a2ca72017
Ramakae <ramakaeletjoi@gmail.com>
2026-02-17 22:35:25 +0200

    notes added

---
+++ Notes.py
@@ -0,0 +1,8 @@
+"""Notes
+Key elements we need here is prices not necessarily description, should I find anomolies with 
+quantity, that is outliers, df.Description analysis will be greatly beneficial to see if these
+were indeed large purchases or large returns and of what good. After that I can keep them
+as there isn't any clear relationship I can see to run a regression analysis.
+Only descriptive analysis can be done here.
+What if I try to find relationship between retention rate and amount of purchases? 
+"""
